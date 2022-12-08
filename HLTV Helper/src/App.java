9252a8ad6d89bf16198d63a9d8c4071df8a1d6e4
HarryRunham <harryrunham@gmail.com>
2022-12-08 11:02:04 +0000

    v0.4.0 - match data storage
    Added system to find the player's matches and store the links to them, along with the player's corresponding rating, in a hash map.
    
    Lowered minimum maps filter on players page to 50 from default 388 - this was useful for testing, as I could use players with far less data. In the future the program will likely start at 500 and iteratively lower the filter as the player is failed to be found.
    
    Small readability improvements.

--- HLTV Helper/src/App.java
+++ HLTV Helper/src/App.java
@@ -1,9 +1,11 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,28 +23,28 @@ public class App {
         Dimension dimensions1 = new Dimension(1000, 700);
         driver.manage().window().setSize(dimensions1); // resize window to be large enough so that the top-right settings dropdown can be seen
         
-        driver.get("https://www.hltv.org/stats/players"); // navigate to this page and pause execution until page is fully loaded
+        driver.get("https://www.hltv.org/stats/players?minMapCount=50"); // navigate to this page and pause execution until page is fully loaded. this page contains all players with 50 or more HLTV-registered maps
         driver.findElement(By.id("CybotCookiebotDialogBodyButtonDecline")).click(); // decline cookies
         Thread.sleep(100); // gives time for site to register choice
-        driver.get("https://www.hltv.org/stats/players"); // page refreshes automatically after cookies option chosen, which can cause stale errors - us refreshing the page and waiting for it to complete refresh is a better option than using a Thread.sleep() etc
+        driver.get("https://www.hltv.org/stats/players?minMapCount=50"); // page refreshes automatically after cookies option chosen, which can cause stale errors - us refreshing the page and waiting for it to complete refresh is a better option than using a Thread.sleep() etc
         
         driver.findElement(By.cssSelector("body > div.navbar > nav > div.navdown > i")).click(); // click settings dropdown toggle to open dropdown
         driver.findElement(By.cssSelector("#popupsettings > div:nth-child(2) > span.right.slider > span.toggleUserTheme.userTheme-night")).click(); // click dark theme - more aesthetically pleasing
         driver.findElement(By.cssSelector("body > div.navbar > nav > div.navdown > i")).click(); // click settings dropdown toggle to close dropdown
         
+        // finding player's match statistics page
+        
         List<WebElement> foundElements = driver.findElements(By.partialLinkText(playerName)); // search for and return every webelement containing a hyperlink with the player's name in. can't just take the first element (using findElement) as a player's name can be contained in another player's name - e.g. nex is contained in nexa, so searching for nex could return nexa's hyperlink element
-
+        
         String acceptedHyperlink = "";
-
+        
         for (WebElement i : foundElements) {
             String hyperlink = i.getAttribute("href"); // grab the hyperlink from the webelement
-            if (hyperlink.length() > 35) { // "https://www.hltv.org/stats/players/" has 35 characters. first of 2 checks to see if the found link is to a player stats page
-                if (hyperlink.substring(0, 35).equals("https://www.hltv.org/stats/players/")) { // second of 2 checks to see if the found link is to a player stats page
-                    String[] hyperlinkSegments = hyperlink.split("/");
-                    if (hyperlinkSegments[hyperlinkSegments.length - 1].equals(playerName.toLowerCase())) { // the ending segment of a HLTV player stats page link is the player's name in lowercase. check if it matches the lowercase version of the player name the user submitted
-                        acceptedHyperlink = hyperlink; // this is the requested player's correct link - the checks ensure that this is e.g. nex's page, not nexa's
-                        break;
-                    }
+            if (hyperlink.length() > 35 && hyperlink.substring(0, 35).equals("https://www.hltv.org/stats/players/")) { // checking if found link is to a player's stats page
+                String[] hyperlinkSegments = hyperlink.split("/");
+                if (hyperlinkSegments[hyperlinkSegments.length - 1].equals(playerName.toLowerCase())) { // the ending segment of a HLTV player stats page link is the player's name in lowercase. check if it matches the lowercase version of the player name the user submitted
+                    acceptedHyperlink = hyperlink; // this is the requested player's correct link - the checks ensure that this is e.g. nex's page, not nexa's
+                    break;
                 }
             }
         }
@@ -51,7 +53,47 @@ public class App {
         String playerID = acceptedHyperlinkSegments[5]; // HLTV has an ID for each player, located in this segment of the hyperlink
         driver.get("https://www.hltv.org/stats/players/matches/" + playerID + "/" + playerName.toLowerCase()); // using the ID we found, access the statistics page for the player's matches
         
-        // strategy probably now: search for match-lost ratingPositive and match-won ratingPositive, grabbing the contents of the tag, and comparing those contents to each other, then for the top 10 ratings, navigate a few <td>s up to get to the <td> with the link in it, then click it
+        // finding elements that could be match rating elements
+        
+        System.out.println("Finding td tag elements");
+        List<WebElement> foundtdElements = driver.findElements(By.tagName("td"));
+        System.out.println("Finished finding td tag elements");
+        
+        // generate matches HashMap by first sifting through foundtdElements for match rating elements
+        
+        HashMap<String, Float> matches = new HashMap<String, Float>(); // key is match hyperlink, value is rating
+        String classValue;
+        Float rating;
+        
+        System.out.println("Populating matches HashMap");
+        
+        for (WebElement i : foundtdElements) {
+            classValue = i.getAttribute("class");
+            if (classValue.equals("match-lost ratingPositive") || classValue.equals("match-won ratingPositive") || classValue.equals("match-lost ratingNegative") || classValue.equals("match-won ratingNegative")) { // rating elements have one of these four class tags
+                
+                try {
+                    rating = Float.parseFloat(i.getText());
+                }
+                catch (NumberFormatException e) {
+                    break; // if the rating isn't a float, we must have a rating from the old rating 1.0 system (indicated with a * after, hence the conversion fail). we will ignore these ratings for now. if we have reached a rating 1.0, every match below will also use rating 1.0 - so we can stop trying to find matches, hence the break
+                }
+                
+                // using relative locators to navigate from the rating element to the element containing the corresponding match hyperlink
+                // this is currently a very inefficient system - I'm sure there are better ways to do this
+                
+                WebElement matchHyperlinkElement1 = driver.findElement(RelativeLocator.with(By.tagName("td")).toLeftOf(i));
+                WebElement matchHyperlinkElement2 = driver.findElement(RelativeLocator.with(By.tagName("td")).toLeftOf(matchHyperlinkElement1));
+                WebElement matchHyperlinkElement3 = driver.findElement(RelativeLocator.with(By.tagName("td")).toLeftOf(matchHyperlinkElement2));
+                WebElement matchHyperlinkElement4 = driver.findElement(RelativeLocator.with(By.tagName("a")).near(matchHyperlinkElement3));
+                String matchHyperlink = matchHyperlinkElement4.getAttribute("href");
+                
+                matches.put(matchHyperlink, rating); // in the future the current plan is for this to only be executed if the rating is top 10 in the ratings discovered so far
+            }
+        }
+        
+        System.out.println("Finished populating matches HashMap");
+        
+        System.out.println(matches); // temporary
         
         userInput.close();
         driver.manage().window().maximize(); // maximise window - most users would do this once the program is finished, so automating it is a good idea
